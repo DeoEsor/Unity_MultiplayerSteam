d8f1cd38468d91f260be15c0bbff725bde011d49
DeoEsor <RulianValeriy@yandex.ru>
2022-09-23 09:50:13 +0300

    Network Property Changed added

---
+++ Assets/Scripts/Infrastructure/Networking/INetworkPropertyChanged.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Infrastructure
+{
+    public interface INetworkPropertyChanged
+    {
+         event Action PropertyChanged;
+
+         void OnPropertyChanged([CallerMemberName] string memberName = null);
+    }
+}
