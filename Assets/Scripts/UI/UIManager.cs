fa64784deb109587d6fe9f2d3946b184ed37a5a4
DeoEsor <RulianValeriy@yandex.ru>
2022-09-24 20:39:29 +0300

    refactored UI Manager

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -4,13 +4,15 @@ using Steamworks;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace UI
 {
     public class UIManager : MonoBehaviour
     {
-        [SerializeField] private GameObject _menu;
+        [FormerlySerializedAs("_menu")] [SerializeField]
+        private GameObject menu;
         private Button _disconnect;
         private Button _exit;
 
@@ -19,20 +21,19 @@ namespace UI
         private PlayerInput _localPlayerInput;
         private RawImage _playerAvatar;
         private Text _playerName;
-        private Button _starthost;
-        private Keyboard keyboard;
+        private Button _startHost;
+        private Keyboard _keyboard;
 
         private void Start()
         {
-            keyboard = Keyboard.current;
+            _keyboard = Keyboard.current;
             _isMenuOn = true;
             _isMenuOnOld = true;
             MainMenuInitializing();
             NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnectedCallback;
             DontDestroyOnLoad(gameObject);
         }
-
-        // Update is called once per frame
+        
         private void Update()
         {
             CheckRunningGame();
@@ -58,14 +59,18 @@ namespace UI
             }
 
             Cursor.visible = _isMenuOn;
-            _menu.SetActive(_isMenuOn);
+            menu.SetActive(_isMenuOn);
         }
 
         private async void InitializingPlayerAvatar()
         {
-            if (!SteamClient.IsValid) return;
+            if (!SteamClient.IsValid) 
+                return;
+            
             var img = await SteamFriends.GetLargeAvatarAsync(SteamClient.SteamId);
-            _playerAvatar.texture = img.Value.Convert();
+            
+            if (img != null) 
+                _playerAvatar.texture = img.Value.Convert();
         }
 
         private PlayerInput FindLocalPlayerInput()
@@ -79,39 +84,43 @@ namespace UI
 
         private void SetLocalPlayerInput(bool boolean)
         {
-            if (_localPlayerInput == null) _localPlayerInput = FindLocalPlayerInput();
+            if (_localPlayerInput == null) 
+                _localPlayerInput = FindLocalPlayerInput();
+            
             if (_localPlayerInput != null)
                 _localPlayerInput.enabled = boolean;
         }
 
         private void MenuUpdate()
         {
-            if (keyboard.escapeKey.wasPressedThisFrame &&
+            if (_keyboard.escapeKey.wasPressedThisFrame &&
                 (NetworkManager.Singleton.IsHost || NetworkManager.Singleton.IsClient))
                 _isMenuOn = !_isMenuOn;
+            
             OpenCloseMenuUpdate();
         }
 
         private void CheckRunningGame()
         {
-            if (!(NetworkManager.Singleton.IsHost || NetworkManager.Singleton.IsClient)) _isMenuOn = true;
+            if (!(NetworkManager.Singleton.IsHost || NetworkManager.Singleton.IsClient)) 
+                _isMenuOn = true;
         }
 
         private void MainMenuInitializing()
         {
-            if (!_menu.transform.Find("Main menu").Find("ButtonStartHost").TryGetComponent(out _starthost))
+            if (!menu.transform.Find("Main menu").Find("ButtonStartHost").TryGetComponent(out _startHost))
                 Debug.LogError("ButtonStartHost not found", this);
             else
-                _starthost.onClick.AddListener(() =>
+                _startHost.onClick.AddListener(() =>
                 {
-                    if (!(NetworkManager.Singleton.IsHost || NetworkManager.Singleton.IsClient))
-                    {
-                        GameNetworkManager.Instance.StartHost();
-                        _isMenuOn = false;
-                    }
+                    if (NetworkManager.Singleton.IsHost || NetworkManager.Singleton.IsClient) 
+                        return;
+                    
+                    GameNetworkManager.Instance.StartHost();
+                    _isMenuOn = false;
                 });
 
-            if (!_menu.transform.Find("Main menu").Find("ButtonDisconnect").TryGetComponent(out _disconnect))
+            if (!menu.transform.Find("Main menu").Find("ButtonDisconnect").TryGetComponent(out _disconnect))
                 Debug.LogError("ButtonDisconnect not found", this);
             else
                 _disconnect.onClick.AddListener(() =>
@@ -120,25 +129,23 @@ namespace UI
                         GameNetworkManager.Instance.Disconnect();
                 });
 
-            if (!_menu.transform.Find("Main menu").Find("ButtonExit").TryGetComponent(out _exit))
+            if (!menu.transform.Find("Main menu").Find("ButtonExit").TryGetComponent(out _exit))
                 Debug.LogError("ButtonExit not found", this);
             else
-                _exit.onClick.AddListener(() => { Application.Quit(); });
+                _exit.onClick.AddListener(Application.Quit);
 
-            if (!_menu.transform.Find("Main menu").Find("PlayerAvatar").TryGetComponent(out _playerAvatar))
+            if (!menu.transform.Find("Main menu").Find("PlayerAvatar").TryGetComponent(out _playerAvatar))
                 Debug.LogError("PlayerAvatar not found", this);
             else
                 InitializingPlayerAvatar();
 
-            if (!_menu.transform.Find("Main menu").Find("PlayerName").TryGetComponent(out _playerName))
+            if (!menu.transform.Find("Main menu").Find("PlayerName").TryGetComponent(out _playerName))
                 Debug.LogError("PlayerName not found", this);
             else
                 _playerName.text = SteamClient.Name;
         }
 
-        private void OnClientConnectedCallback(ulong clientId)
-        {
-            _isMenuOn = false;
-        }
+        private void OnClientConnectedCallback(ulong clientId) 
+            => _isMenuOn = false;
     }
 }
