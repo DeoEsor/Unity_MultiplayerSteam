a8ba7fecd8f8b5a6a23c215875bd331241507114
DeoEsor <RulianValeriy@yandex.ru>
2022-09-24 20:35:27 +0300

    refactored BasicRigidBodyPush.cs

--- Assets/Scripts/Physics/BasicRigidBodyPush.cs
+++ Assets/Scripts/Physics/BasicRigidBodyPush.cs
@@ -18,25 +18,22 @@ namespace Physics
 
         private void PushRigidBodies(ControllerColliderHit hit)
         {
-            // https://docs.unity3d.com/ScriptReference/CharacterController.OnControllerColliderHit.html
-
-            // make sure we hit a non kinematic rigidbody
             var body = hit.collider.attachedRigidbody;
+            
             if (body == null || body.isKinematic) 
                 return;
 
-            // make sure we only push desired layer(s)
+            
             var bodyLayerMask = 1 << body.gameObject.layer;
+            
             if ((bodyLayerMask & pushLayers.value) == 0) 
                 return;
-
-            // We dont want to push objects below us
-            if (hit.moveDirection.y < -0.3f) return;
-
-            // Calculate push direction from move direction, horizontal motion only
+            
+            if (hit.moveDirection.y < -0.3f) 
+                return;
+            
             var pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
 
-            // Apply the push and take strength into account
             body.AddForce(pushDir * strength, ForceMode.Impulse);
         }
     }
