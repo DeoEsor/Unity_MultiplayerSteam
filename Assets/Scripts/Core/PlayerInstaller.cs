017684b5c4d5baf4fd5e9e56df1122eaee9cd3e6
DeoEsor <RulianValeriy@yandex.ru>
2022-09-25 19:12:13 +0300

    player installer added

---
+++ Assets/Scripts/Core/PlayerInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure;
+using Zenject;
+
+namespace Core
+{
+    public class PlayerInstaller : MonoInstaller<PlayerInstaller>
+    {
+        public override void InstallBindings()
+        {
+            //Container.Bind<Interface>().To<Implementation>().AsSingle();
+            //Container.Bind<IManager>().ToSelf().As
+        }
+    }
+}
