a8d816c2367ad3ce1d6763b510c56e4621e9e598
DeoEsor <RulianValeriy@yandex.ru>
2022-09-29 01:58:13 +0300

    added patrol state machine

---
+++ Assets/Scripts/AI/StateMachines/PatrolStateMachine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Core;
+using Infrastructure.Core;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace AI.StateMachines
+{
+    [Serializable]
+    public class PatrolStateMachine : StateMachineBase
+    {
+        public Transform[] wayPoints;
+
+        [SerializeField]
+        private string animationName = "Move";
+        
+        public int currentPatch;
+
+        public override void Enter()
+        { }
+
+        public override void HandleInput()
+        { }
+
+        public override void LogicUpdate()
+        {
+            AIAgent.isStopped = false;
+            Animator.SetBool(animationName, true);
+                    
+            AIAgent.SetDestination(wayPoints[currentPatch].transform.position);
+                    
+            var patchDist = Vector3.Distance(wayPoints[currentPatch].transform.position, transform.position);
+
+            if (!(patchDist < 2)) return;
+            
+            currentPatch++;
+            currentPatch %= wayPoints.Length;
+        }
+
+        public override void PhysicsUpdate()
+        { }
+
+        public override void Exit()
+        { }
+    }
+}
