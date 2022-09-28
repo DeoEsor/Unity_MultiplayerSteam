7dfb2ecf5d2e0efe01dd757b0a1587fcd41ed155
DeoEsor <RulianValeriy@yandex.ru>
2022-09-28 22:51:37 +0300

    init layer helper

---
+++ Assets/Scripts/Core/LayerHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+namespace Core
+{
+    public static class LayerHelper
+    {
+        private static readonly Dictionary<SteamId, LayerMask> PlayerLayerMasks = new();
+        
+        /// <summary>
+        /// Returns player layer mask
+        /// </summary>
+        /// <param name="playerId">player steam id</param>
+        /// <returns>player layer mask if </returns>
+        public static LayerMask GetPlayerMask(SteamId playerId) 
+            => PlayerLayerMasks.ContainsKey(playerId) 
+                ? PlayerLayerMasks[playerId] 
+                : -1;
+
+        public static void AddPlayer(SteamId playerId)
+        {
+            if (PlayerLayerMasks.ContainsKey(playerId))
+            {
+                throw new ArgumentException("Same user already exists", nameof(playerId));
+            }
+            PlayerLayerMasks.Add(playerId, new LayerMask());
+        }
+        
+        public static void RemovePlayer(SteamId playerId)
+        {
+            if (PlayerLayerMasks.ContainsKey(playerId))
+                PlayerLayerMasks.Remove(playerId);
+        }
+    }
+}
