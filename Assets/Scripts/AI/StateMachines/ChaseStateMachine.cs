8467ddc894b20f8c22d489cc2b9ab9b0d5219ccd
DeoEsor <RulianValeriy@yandex.ru>
2022-09-29 01:57:59 +0300

    added chase state machine

---
+++ Assets/Scripts/AI/StateMachines/ChaseStateMachine.cs
@@ -0,0 +1,41 @@
+﻿using Core;
+using UnityEngine;
+
+namespace AI.StateMachines
+{
+    public class ChaseStateMachine : StateMachineBase
+    {
+        [SerializeField]
+        private string animationName = "Move";
+        
+        public GameObject Player { get; set; }
+        
+        public override void Enter()
+        { }
+        
+        /// <summary>
+        /// Use this instead empty enter
+        /// </summary>
+        /// <param name="player"></param>
+        public void Enter(GameObject player)
+        {
+            Player = player;
+            Enter();
+        }
+
+        public override void HandleInput()
+        { }
+
+        public override void LogicUpdate()
+        {
+            Animator.SetBool(animationName, true);
+            AIAgent.SetDestination(Player.transform.position);
+        }
+
+        public override void PhysicsUpdate()
+        { }
+
+        public override void Exit()
+        { }
+    }
+}
