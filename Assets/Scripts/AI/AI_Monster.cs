567eef379a13649e3b8c18d44c95ae37a6aa34db
DeoEsor <RulianValeriy@yandex.ru>
2022-09-29 01:58:51 +0300

    AI monster restructed with state pattern

--- Assets/Scripts/AI/AI_Monster.cs
+++ Assets/Scripts/AI/AI_Monster.cs
@@ -1,69 +1,72 @@
 ﻿using System;
+using AI.StateMachines;
+using Infrastructure.Core;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace AI
 {
-    public partial class AIMonster : MonoBehaviour
+    [RequireComponent(typeof(ChaseStateMachine), 
+        typeof(StayStateMachine), 
+        typeof(PatrolStateMachine))]
+    public partial class AIMonster : MonoBehaviour, IStateObject
     {
         private NavMeshAgent _aiAgent;
         private GameObject _player;
-        public GameObject panelGaveOver;
         private Animator _animator;
 
-        public Transform[] wayPoints;
-        public int currentPatch;
-
         public AIState aiEnemy;
+        public IStateMachine CurrentState { get; set; }
 
         private void Start()
         {
             _animator = gameObject.GetComponent<Animator>();
             _aiAgent = gameObject.GetComponent<NavMeshAgent>();
-            _player = GameObject.FindGameObjectWithTag("Player");
+            Initialize(gameObject.GetComponent<ChaseStateMachine>());
         }
+
+        private void Update() => CurrentState.LogicUpdate();
 
         private void FixedUpdate()
         {
-            switch (aiEnemy)
-            {
-                case AIState.Patrol:
-                {
-                    _aiAgent.isStopped = false;
-                    _animator.SetBool("Move", true);
-                    
-                    _aiAgent.SetDestination(wayPoints[currentPatch].transform.position);
-                    
-                    var patchDist = Vector3.Distance(wayPoints[currentPatch].transform.position, gameObject.transform.position);
-                    
-                    if (patchDist < 2)
-                    {
-                        currentPatch++;
-                        currentPatch = currentPatch % wayPoints.Length;
-                    }
-
-                    break;
-                }
-                case AIState.Stay:
-                    _animator.SetBool("Move", false);
-                    _aiAgent.isStopped = true;
-                    break;
-                case AIState.Chase:
-                    _animator.SetBool("Move", true);
-                    _aiAgent.SetDestination(_player.transform.position);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-
-
-            var distPlayer = Vector3.Distance(_player.transform.position, gameObject.transform.position);
+            var distPlayer = Vector3.Distance(_player.transform.position, transform.position);
             
             if (!(distPlayer < 2)) 
                 return;
             
-            _player.SetActive(false);
-            panelGaveOver.SetActive(true);
+            // TODO Attack logic
+        }
+        public void Initialize(IStateMachine startingState)
+        {
+            CurrentState = startingState;
+            startingState.Enter();
+        }
+        
+        public void ChangeState(IStateMachine newState)
+        {
+            CurrentState.Exit();
+
+            CurrentState = newState;
+            newState.Enter();
+        }
+
+        public void ChangeState(AIState state)
+        {
+            switch (state)
+            {
+                case AIState.Patrol:
+                    ChangeState(new PatrolStateMachine());
+                    break;
+                case AIState.Stay:
+                    ChangeState(new PatrolStateMachine());
+                    break;
+                case AIState.Chase:
+                    ChangeState(new PatrolStateMachine());
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            throw new NotImplementedException();
         }
     }
 }
