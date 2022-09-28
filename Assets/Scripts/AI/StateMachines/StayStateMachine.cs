8301de623560dc9b9def386c6debb1a913d3f8a2
DeoEsor <RulianValeriy@yandex.ru>
2022-09-29 01:58:24 +0300

    added stay state machine

---
+++ Assets/Scripts/AI/StateMachines/StayStateMachine.cs
@@ -0,0 +1,32 @@
+﻿using Core;
+using Infrastructure.Core;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
+
+namespace AI.StateMachines
+{
+    public class StayStateMachine : StateMachineBase
+    {
+        [SerializeField]
+        private string animationName = "Move";
+        
+        public override void Enter()
+        {
+            Animator.SetBool(animationName, false);
+            AIAgent.isStopped = true;
+        }
+
+        public override void HandleInput()
+        { }
+
+        public override void LogicUpdate()
+        { }
+
+        public override void PhysicsUpdate()
+        { }
+
+        public override void Exit()
+        { }
+    }
+}
