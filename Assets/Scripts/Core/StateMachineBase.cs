88b18d7b18e19578b0c9947fb7938800982832ad
DeoEsor <RulianValeriy@yandex.ru>
2022-09-29 01:57:21 +0300

    added state machine base

---
+++ Assets/Scripts/Core/StateMachineBase.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Core;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Core
+{
+    public abstract class StateMachineBase : MonoBehaviour, IStateMachine
+    {
+        public NavMeshAgent AIAgent { get; set; }
+        public Animator Animator { get; set; }
+        
+        public IStateObject _controllerObject;
+        
+        [SerializeField]
+        private AudioClip stepsSound;
+
+        public abstract void Enter();
+
+        public abstract void HandleInput();
+
+        public abstract void LogicUpdate();
+        
+        public abstract void PhysicsUpdate();
+
+        public abstract void Exit();
+    }
+}
