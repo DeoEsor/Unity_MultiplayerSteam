c44671951b081ed089f6c03e3136f7796945b4e0
DeoEsor <RulianValeriy@yandex.ru>
2022-09-29 01:56:27 +0300

    IStateMachine.cs state machine interface added

---
+++ Assets/Scripts/Infrastructure/Core/IStateMachine.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Infrastructure.Core
+{
+    public interface IStateMachine
+    {
+        NavMeshAgent AIAgent { get; set; }
+        Animator Animator { get; set; }
+        public void Enter();
+
+        public void HandleInput();
+
+        public void LogicUpdate();
+
+        public void PhysicsUpdate();
+
+        public void Exit();
+    }
+}
