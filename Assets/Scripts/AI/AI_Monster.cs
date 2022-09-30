90727acac159ff37d061c8194671b88490085e16
DeoEsor <RulianValeriy@yandex.ru>
2022-09-30 23:37:42 +0300

    moved all State pattern to MonsterStateObject class

--- Assets/Scripts/AI/AI_Monster.cs
+++ Assets/Scripts/AI/AI_Monster.cs
@@ -1,89 +1,14 @@
-﻿using System;
-using AI.StateMachines;
-using Infrastructure.Core;
+﻿using Infrastructure.Core;
 using UnityEngine;
-using UnityEngine.AI;
+using Zenject;
 
 namespace AI
 {
-    [RequireComponent(typeof(ChaseStateMachine), 
-        typeof(StayStateMachine), 
-        typeof(PatrolStateMachine))]
-    public partial class AIMonster : MonoBehaviour, IStateObject
+    public partial class AIMonster : MonoBehaviour
     {
-        private NavMeshAgent _aiAgent;
         private GameObject _player;
-        private Animator _animator;
-
-        public AIState aiEnemy;
-        public IStateMachine CurrentState { get; set; }
-
-        private void Start()
-        {
-            _animator = gameObject.GetComponent<Animator>();
-            _aiAgent = gameObject.GetComponent<NavMeshAgent>();
-            Initialize(gameObject.GetComponent<ChaseStateMachine>());
-        }
-
-        private void Update() => CurrentState.LogicUpdate();
-
-        private void FixedUpdate()
-        {
-            var distPlayer = Vector3.Distance(_player.transform.position, transform.position);
-            
-            if (!(distPlayer < 2)) 
-                return;
-            
-            // TODO Attack logic
-        }
-        public void Initialize(IStateMachine startingState)
-        {
-            CurrentState = startingState;
-            startingState.Enter();
-        }
         
-        public void ChangeState(IStateMachine newState)
-        {
-            CurrentState.Exit();
-
-            CurrentState = newState;
-            newState.Enter();
-        }
-
-        public void ChangeState(byte newStateFlag)
-        {
-            switch ((AIState) newStateFlag)
-            {
-                case AIState.Patrol:
-                    ChangeState(new PatrolStateMachine());
-                    break;
-                case AIState.Stay:
-                    ChangeState(new PatrolStateMachine());
-                    break;
-                case AIState.Chase:
-                    ChangeState(new PatrolStateMachine());
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-
-        public void ChangeState(AIState state)
-        {
-            switch (state)
-            {
-                case AIState.Patrol:
-                    ChangeState(new PatrolStateMachine());
-                    break;
-                case AIState.Stay:
-                    ChangeState(new PatrolStateMachine());
-                    break;
-                case AIState.Chase:
-                    ChangeState(new PatrolStateMachine());
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
+        [Inject]
+        private IStateObject _monsterStateObject;
     }
 }
