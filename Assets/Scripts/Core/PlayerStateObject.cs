fe2d691b2cfe608cc5379cafec4e8ab984803063
DeoEsor <RulianValeriy@yandex.ru>
2022-09-30 23:40:42 +0300

    init player state object class

---
+++ Assets/Scripts/Core/PlayerStateObject.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure.Core;
+
+namespace Core
+{
+    public class PlayerStateObject : IStateObject
+    {
+        public IStateMachine CurrentState { get; set; }
+        
+        public void Initialize(IStateMachine startingState)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ChangeState(IStateMachine newState)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ChangeState(byte newStateFlag)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
