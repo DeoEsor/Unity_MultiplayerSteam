29f6b93d670a663b89dcb19f6306d088c249b24b
DeoEsor <RulianValeriy@yandex.ru>
2022-10-01 21:26:10 +0300

    added Door interaction class

---
+++ Assets/Scripts/Environment/Door.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructure.Environment;
+using UnityEngine;
+
+namespace Environment
+{
+    public class Door : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private Animator mAnimator;
+        [SerializeField] private bool isOpen;
+        private static readonly int IsOpen = Animator.StringToHash("isOpen");
+        
+        [SerializeField] private string descriptionOnEnabled = "Press [E] to <color=green>open</color> the door.";
+        [SerializeField] private string descriptionOnDisable = "Press [E] to <color=red>close</color> the door.";
+
+        private void Start()
+        {
+            if (isOpen)
+                mAnimator.SetBool(IsOpen, true);
+        }
+
+        public string GetDescription() 
+            => isOpen ? descriptionOnDisable : descriptionOnEnabled;
+
+        public void Interact()
+        {
+            isOpen = !isOpen;
+            mAnimator.SetBool(IsOpen, isOpen);
+        }
+    }
+}
