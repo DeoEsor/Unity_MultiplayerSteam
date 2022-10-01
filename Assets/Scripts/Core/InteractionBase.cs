d62433c137b8fb6c84cdcf0d92ef9e6f2fc6bb5a
DeoEsor <RulianValeriy@yandex.ru>
2022-10-01 21:56:32 +0300

    init InteractionBase.cs

---
+++ Assets/Scripts/Core/InteractionBase.cs
@@ -0,0 +1,46 @@
+﻿using Infrastructure.Environment;
+using Infrastructure.Player;
+using TMPro;
+using UnityEngine;
+
+namespace Core.Core
+{
+    public abstract class InteractionBase :  IInteraction<InteractionBase>
+    {
+        [SerializeField]  private Camera _mainCam;
+        [SerializeField] private readonly float interactionDistance = 10f;
+
+        [SerializeField] private GameObject interactionUI;
+        [SerializeField] private TextMeshProUGUI interactionText;
+
+        private void Update()
+        {
+            InteractionRay();
+        }
+
+        public abstract InteractionBase GetInteractData();
+
+        public void InteractionRay()
+        {
+            var ray = _mainCam.ViewportPointToRay(Vector3.one / 2f);
+
+            if (!Physics.Raycast(ray, out var hit, interactionDistance))
+            {
+                interactionUI.SetActive(false);
+                return;
+            }
+            
+            var interactable = hit.collider.GetComponent<IInteractable>();
+
+            if (interactable != null)
+            {
+                interactionText.text = interactable.GetDescription();
+
+                if (Input.GetKeyDown(KeyCode.E)) 
+                    interactable.Interact();
+            }
+            
+            interactionUI.SetActive(true);
+        }
+    }
+}
